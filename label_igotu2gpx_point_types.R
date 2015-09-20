aa5857393212a20e3325a9154e20560022eaf32d
thomasevans <thomas.jude.evans@gmail.com>
2015-09-20 20:52:47 +0200

    classifying the igu data

--- label_igotu2gpx_point_types.R
+++ label_igotu2gpx_point_types.R
@@ -14,8 +14,8 @@ library("RODBC")
 gps.db <- odbcConnectAccess2007('D:/Dropbox/tracking_db/murre_db/murre_db.accdb')
 
 # Get GPS data
-points <- sqlQuery(gps.db,
-                   query = "SELECT g.device_info_serial, g.date_time, g.latitude, g.longitude, g.speed_ms, g.timeout, g.ehpe, guillemots_track_session.ring_number
+gpspoints <- sqlQuery(gps.db,
+                   query = "SELECT g.device_info_serial, g.date_time, g.latitude, g.longitude, g.speed_ms, g.timeout, g.ehpe, g.sat_n, guillemots_track_session.ring_number
 FROM guillemots_track_session, guillemots_gps_points_igu AS g
                    WHERE (((g.date_time)>=[guillemots_track_session].[start_date] And (g.date_time)<=[guillemots_track_session].[end_date]) AND ((guillemots_track_session.device_info_serial)=g.device_info_serial))
                    ORDER BY g.device_info_serial, g.date_time;",
@@ -25,19 +25,36 @@ FROM guillemots_track_session, guillemots_gps_points_igu AS g
 # Label by whether vallid GPS fix -----
 
 # Has location?
-gps_val <- (points$long != 0) & (points$lat != 0)
+gps_val <- (gpspoints$long != 0) & (gpspoints$lat != 0)
 summary(gps_val)
 
 # Is good quality GPS fix?
-f0 <- gps_val & (points$timeout <= 100)
-f1 <- gps_val & (points$ehpe <= 100)
-gps_ok <- f0 & f1
+f0 <- gps_val & (gpspoints$timeout <= 100)
+f1 <- gps_val & (gpspoints$ehpe <= 200)
+# Looks to be that these points shouldn't be excluded, though they are odd, as have zero satellites!
+f2 <- ((gpspoints$ehpe >= 200) & gps_val & 
+         (gpspoints$timeout != 250)  & (gpspoints$sat_n == 0))
+gps_ok <- (f0 & f1) | f2
 summary(gps_ok)
 
 # Look at those flagged as NA - appear to be only the 2009 data
 # which were not downloaded with the igotu2gpx program
-points2 <- points[is.na(gps_ok),]
-unique(points2$device_info_serial)
+gpspoints2 <- gpspoints[is.na(gps_ok),]
+unique(gpspoints2$device_info_serial)
+
+
+# 2009 points, whichc were downloaded with @trip, thus don't have
+# the detailed information will be classified as ok
+gps_ok[is.na(gps_ok)] <- TRUE
+# 
+
+# points.ehpe.bad2 <- gpspoints[f2,]
+# points.ehpe.bad2 <- points.ehpe.bad[points.ehpe.bad$sat_n == 0,]
+
+
+# hist(gpspoints$ehpe, breaks = 100)
+
+
 
 
 # Label by 'behaviour' ------
@@ -50,11 +67,11 @@ unique(points2$device_info_serial)
 # 1 . Flight ----
 # label as flight if instantaneous speed exceeds
 # 5 ms-1
-flight <- points$speed_ms > 5
+flight <- gpspoints$speed_ms > 5
 summary(flight)
 
 # # Check above looks sensible
-# hist(points$speed_ms, ylim = c(0,2000), breaks = 100)
+# hist(gpspoints$speed_ms, ylim = c(0,2000), breaks = 100)
 # abline(v=5)
 
 # 2. Collony ----
@@ -76,43 +93,51 @@ fun.dist <- function(lat,long){
 }
 
 # Get distance for each point
-col.dist <- mapply(fun.dist,points$lat,points$long)
+col.dist <- mapply(fun.dist,gpspoints$lat,gpspoints$long)
 
 # Convert distance from km to m
 col.dist <- col.dist * 1000
 
-# Have a look at this data
-hist(col.dist)
-hist(col.dist, breaks = 40)
-hist(col.dist, breaks = 5000, xlim = c(0, 500))
-# Appears that distances between ca. 0 and 100
-# correspond to murre-lab - i.e. nesting - and
-# distances from ca. 100 - 200 are the office (i.e.
-# non-deployed loggers)
-# Non-deployed logger locations should later be 
-# removed by only including data from during the
-# the deployment period.
-
-# Locations within 100 m of central location in colony
-col.loc <- col.dist < 100
+# Locations within 200 m of central location in colony
+col.loc <- col.dist < 200
 summary(col.loc)
+
+# 
+# # Have a look at this data
+# hist(col.dist)
+# hist(col.dist, breaks = 40)
+# hist(col.dist, breaks = 5000, xlim = c(0, 500))
+# # Appears that distances between ca. 0 and 200
+# # correspond to murre-lab 
+# 
+# # See where these locations falling between 100 - 200 m are located
+# # First plot all locations within 500 m of colony:
+# m500 <- col.dist < 500
+# plot(gpspoints$longitude[m500], gpspoints$latitude[m500])
+# 
+# m100 <- col.dist < 100
+# points(gpspoints$longitude[m100], gpspoints$latitude[m100], col = "grey")
+# m100_200 <- (col.dist > 100) & (col.dist < 200)  
+# points(gpspoints$longitude[m100_200], gpspoints$latitude[m100_200], col = "blue")
+# # colony location
+# points(17.958252, 57.289848, col = "red", cex = 5)
 
 
 # 3. Diving (apparent) ----
-diving <- ((points$lat == 0 ) | (points$long == 0 ) )& (points$timeout == 12)
+diving <- ((gpspoints$lat == 0 ) | (gpspoints$long == 0 ) )& (gpspoints$timeout == 12)
 summary(diving)
 
-# View number of diving fixes by hour of day
-hours <- as.POSIXlt(points$date_time[diving],
-                    tz = "UTC")
-hours.loc <- as.POSIXlt(hours, tz = "Europe/Stockholm")
-
-
-hist(((as.numeric(as.POSIXlt(hours.loc)$hour)+2) %%24), xlim = c(0,24), breaks = 24,
-     xlab = "Hour of day (UTC)",
-     ylab = "Diving GPS fixes",
-     main = "",
-     col = "grey60")
+# # View number of diving fixes by hour of day
+# hours <- as.POSIXlt(gpspoints$date_time[diving],
+#                     tz = "UTC")
+# hours.loc <- as.POSIXlt(hours, tz = "Europe/Stockholm")
+# 
+# 
+# hist(((as.numeric(as.POSIXlt(hours.loc)$hour)+2) %%24), xlim = c(0,24), breaks = 24,
+#      xlab = "Hour of day (UTC)",
+#      ylab = "Diving GPS fixes",
+#      main = "",
+#      col = "grey60")
 
 
 # Some labelling of previous/ next points? -----
@@ -123,7 +148,7 @@ hist(((as.numeric(as.POSIXlt(hours.loc)$hour)+2) %%24), xlim = c(0,24), breaks =
 
 # Label points where next fix is diving
 # Index thing
-n <- length(points$lat)
+n <- length(gpspoints$lat)
 # Make an index
 ind <- c(1:n)
 
@@ -159,11 +184,16 @@ type[diving] <- "dive"
 # no loc, not diving
 summary(!gps_val)
 summary(diving)
-points.test <- points[(!gps_val & !diving),]
+points.test <- gpspoints[(!gps_val & !diving),]
 type[(!gps_val & !diving)] <- "no_location"
 
+# get 'map.trip' function
+source("map.trip.R")
+
 # View these points on map
-map.trip(points = points[gps_ok,])
+# map.trip(points = gpspoints[gps_ok,], xlim = c(17.7,18.1),
+         # ylim = c(57.2, 57.4))
+map.trip(points = gpspoints[gps_ok,])
 
 #missing points, not diving
 ind.miss <- ind[(!gps_val & !diving)]
@@ -171,9 +201,9 @@ ind.miss <- ind[(!gps_val & !diving)]
 # Previous point
 ind.miss.prev <- ind.miss -1
 
-points2plot <- ind.miss.prev[points$long[ind.miss.prev] != 0]
+points2plot <- ind.miss.prev[gpspoints$long[ind.miss.prev] != 0]
 
-points(points$lat[points2plot]~points$long[points2plot],
+points(gpspoints$lat[points2plot]~gpspoints$long[points2plot],
        col = "green", pch = 8)
 # Appear to be nearly universally colony based locations.
 # Removing these then won't matter too much - not parts of
@@ -184,7 +214,7 @@ summary(as.factor(type))
 # With a location
 # With a location but bad
 bad_loc <- (gps_val & !gps_ok)
-points(points$lat[bad_loc]~points$long[bad_loc],
+points(gpspoints$lat[bad_loc]~gpspoints$long[bad_loc],
        col = "magenta", pch = 8)
 type[bad_loc] <- "bad_location"
 
@@ -197,16 +227,31 @@ summary(as.factor(type))
 
 
 # Colony
-col.points <- col.loc & !bad_loc
+# Could exclude bad locations here - but really they are probably colony if located there
+# col.points <- col.loc & !bad_loc
+col.points <- col.loc
 summary(col.points)
 
 type[col.points] <- "colony"
+summary(as.factor(type))
 
 
 # Map these points
-points(points$lat[col.points]~points$long[col.points],
+points(gpspoints$lat[col.points]~gpspoints$long[col.points],
        col = "orange", pch = 8)
 
+points(gpspoints$lat[type == "unclassified"]~gpspoints$long[type == "unclassified"],
+       col = "light blue")
+
+# Two obvious outlier points
+type[gpspoints$lat > 57.7] <- "bad_location"
+
+# points(points.ehpe.bad2$longitude, points.ehpe.bad2$latitude, col = "orange",
+# cex = 5, pch= 8)
+# 
+# x <- points.ehpe.bad2$timeout == 250
+# points(points.ehpe.bad2$longitude[x], points.ehpe.bad2$latitude[x], col = "black",
+#        cex = 5, pch= 8)
 
 # Remaining points - both water surface and pre/ post deployment
 # points - will have to remove those later (by deployment period)
@@ -214,13 +259,22 @@ points(points$lat[col.points]~points$long[col.points],
 # Later can classify into 'surface' and just filter out the 
 # non-deployment points
 
+type[type == "unclassified"] <- "surface"
+
+
+# # 2009 points
+# gpspoints3 <- cbind(gpspoints, type)
+# gpspoints2 <- gpspoints3[is.na(gps_ok),]
+# unique(gpspoints2$device_info_serial)
+# unique(gpspoints2$type)
+
 
 
 # Output to new DB table ----
 
 # Assemble vairables to new data frame
-export.table <- cbind(points$device_info_serial,
-                      points$date_time)
+export.table <- cbind(gpspoints$device_info_serial,
+                      gpspoints$date_time)
 
 # Add colony distance
 export.table <- cbind(export.table, col.dist)
@@ -255,8 +309,6 @@ str(export.table)
 
 
 # Write to database
-library("RODBC")
-gps.db <- odbcConnectAccess2007('D:/Dropbox/tracking_db/GPS_db.accdb')
 
 
 #will be neccessary to edit table in Access after to define data-types and primary keys and provide descriptions for each variable.
