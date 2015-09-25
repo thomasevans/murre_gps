98e35dc7378b545912e31c8e16024be881c766b3
Tom Evans <thomas.jude.evans@gmail.com>
2015-09-25 13:21:57 +0200

    Parse data from Movebank - weather data - then output to DB

---
+++ parse_movebank_to_db.R
@@ -0,0 +1,17 @@
+# Script to parse data from movebank - then put this into Access DB
+
+
+# Import data from text file ------
+
+
+
+# Delete unrequired columns -----
+
+
+# Fix data structure -------
+
+
+# Fix names ----
+
+
+# Output to DB -----
