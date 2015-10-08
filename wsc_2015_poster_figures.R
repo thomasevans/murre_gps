b474a6394c7f513fa29453bd193d08e24345096e
Tom Evans <thomas.jude.evans@gmail.com>
2015-10-08 10:46:29 +0200

    Figures relating to GPS data for WSC poster

---
+++ wsc_2015_poster_figures.R
@@ -0,0 +1,17 @@
+
+# Figures for WSC murre foraging poster - for GPS data
+
+#' 1. Maps for 3 years of foraging tracks
+#' 2. Histogram of distance from colony (foraging locations only, weighted by time-interval between GPS fixes)
+#' 3. Histogram of water depth at foraging 'locations'
+#'    - Need to then extract water depth for all GPS location - do in new script
+
+
+
+# Get GPS data for murres for the 3 years - linked with foraging trip ID ------
+#' Paramaters required:
+#' - Device_info_serial
+#' - Date_time
+#' - Lat + long
+#' - Distance from colony
+#' - 
