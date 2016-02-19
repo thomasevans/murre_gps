c41f1750f24eb72efa6496f8f4138903c77a1191
Tom Evans <thomas.jude.evans@gmail.com>
2016-02-19 09:25:04 +0100

    Example of how to use function to process igotu2gpx raw file output

---
+++ parse_igotu2gpx_txt_example.R
@@ -0,0 +1,18 @@
+# Example of how to use functions in 'parse_igotu2gpx_txt.R'
+
+
+# Source the functions from that file (assumes it is in your working directory)
+source("parse_igotu2gpx_txt.R")
+
+# Process an example file
+gps_data <- parse.file("g11_details.txt")
+
+# View the generated dataframe:
+View(gps_data)
+
+# See the data structure
+str(gps_data)
+
+
+# The dataframe can be output to a csv file or similar:
+write.csv(gps_data, file = "g11_details_processed.csv" )
